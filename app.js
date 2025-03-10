64a9ff99d5bdc79c8361d16a5b671275ed4427b0
Yongji-Li <192184280+Yongji-Li@users.noreply.github.com>
2025-03-10 10:47:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('access');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Chuden!!-onepod-0309');
+  res.end('Hello Chuden!!-onepod-0310');
 });
 
 server.listen(8080, () => {
